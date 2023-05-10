c58788e9c050047d9cf62eaa3cb68bc9e18f7f8b
Sávio Silva Moreira <saviodiowflamengo@gmail.com>
2023-05-10 11:39:28 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,42 @@
+from django.views.generic import TemplateView
+from rest_framework import generics
+
+from core.models import Department, Employee
+from core.serializers import DepartmentSerializer, EmployeeRegistrationSerializer, EmployeeReadOnlySerializer
+
+
+class HomeView(TemplateView):
+    departments = Department.objects.all()
+    employees = Employee.objects.all()
+
+    extra_context = {'departments': departments, 'employees': employees}
+
+    template_name = 'core/home.html'
+
+
+class DepartmentList(generics.ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class EmployeeList(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method.upper() == "GET":
+            return EmployeeReadOnlySerializer
+        return EmployeeRegistrationSerializer
+
+
+class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method.upper() == "GET":
+            return EmployeeReadOnlySerializer
+        return EmployeeRegistrationSerializer
